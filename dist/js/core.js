b609fddd0a9a6c8d2b826ba7ded775d80c22ee34
anjanagihan666 <ajgihan@gmail.com>
2018-03-26 15:56:58 +0530

    deployed dist folder

--- dist/js/core.js
+++ dist/js/core.js
@@ -7,4 +7,24 @@ var GetStoredObject = function (name) {
     return localStorage.getItem(name);
 }
 
+var HideShowSideBar = function () {
+    if ($("#divSideBar").hasClass("sidebar-collapsed")) {
+        $("#divSideBar").removeClass("sidebar-collapsed");
+        $("main").removeClass("mg-left-45")
+    } else { 
+        $("#divSideBar").addClass("sidebar-collapsed");
+        $("main").addClass("mg-left-45")
+    }
+}
+
+var HideShowMobileSideBar = function () {
+    if ($("#divSideBar").hasClass("sidebar-collapsed-mobile")) {
+        $("#divSideBar").removeClass("sidebar-collapsed-mobile");
+        $("main").removeClass("mg-left-45")
+    } else {
+        $("#divSideBar").addClass("sidebar-collapsed-mobile");
+        $("main").removeClass("mg-left-45")
+    }
+}
+
 SetStoredObject('restApiPath', 'https://my-dashboard-restapi.herokuapp.com/');
